dbfd89123d9c3633e6697baafb4b1d305a3a8cfc
Vivek Jaiswal <vivekjai10@gmail.com>
2020-12-25 17:13:38 +0530

    Update Constants.java
    removed email and password

--- app/src/main/java/edu/sinhgad/submitassignmentsit/Constants.java
+++ app/src/main/java/edu/sinhgad/submitassignmentsit/Constants.java
@@ -1,9 +1,10 @@
+// This is not a good practice to store your data
 package edu.sinhgad.submitassignmentsit;
 
 public class Constants {
 
-    public static final String MAIN_EMAIL = "vivek@submitassignmentsit.xyz";
-    public static final String MAIN_PASSWORD = "qKavPQF8XjDT";
+    public static final String MAIN_EMAIL = ""; // email address
+    public static final String MAIN_PASSWORD = "" // password;
     public static final String WELCOME_SUBJECT = "Welcome to Submit Assignment SIT.";
     public static final String WELCOME_MESSAGE = "\n\nI hope you are having a good day." +
             "\n\nAs our app is still in developing phase, you may encounter some errors. If you do encounter any error please report it using the Support option provided within the app" +
