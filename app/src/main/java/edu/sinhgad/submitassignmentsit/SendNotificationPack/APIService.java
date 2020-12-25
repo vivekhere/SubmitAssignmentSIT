fc349df1f8ad7b43aca1d2dfdd215f1f6d9ba729
Vivek Jaiswal <vivekjai10@gmail.com>
2020-12-25 17:15:52 +0530

    Update APIService.java
    removed server key

--- app/src/main/java/edu/sinhgad/submitassignmentsit/SendNotificationPack/APIService.java
+++ app/src/main/java/edu/sinhgad/submitassignmentsit/SendNotificationPack/APIService.java
@@ -9,7 +9,7 @@ public interface APIService {
     @Headers(
             {
                     "Content-Type: application/json",
-                    "Authorization: key = AAAAY7-xdH4:APA91bGvfEj27-ab7s3tn7ljW3rmUs9jkXEkH3jJ0dpWmSOEEodkmP74MXivWArUpQuCp8OaRoZL3GCU8BIi-ZQKm4UlTeHcs2AF4WSF6AZyRIJJrL4dQ6wTTVgRCQlmC1NH66oev5Ay"
+                    "Authorization: key = AAAAY7-xdH4:...." //Server key
             }
     )
 
